607216cb6d75e8b53dca61b2f455ce5e24fefc02
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2025-07-16 23:16:14 -0400

    much more state management has been done

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,17 @@ mod wakeonlan;
 use wakeonlan::wakeonlan;
 mod mac;
 
-use log::{LevelFilter, debug, error, info};
+use log::{LevelFilter, debug, error, info, warn};
 
 use std::io::{Error, ErrorKind};
 use std::thread::sleep;
-use std::time::{Duration, Instant};
+use std::time::{Duration, Instant, SystemTime};
 
 use crate::monitoring::UPSStatus;
+use crate::state::{
+    DeviceState, NutjobState, get_state, init_state, mark_device_online, mark_online_devices,
+    save_state, update_ups_state,
+};
 
 fn string_to_level_filter(log_level: &String) -> Result<LevelFilter, Error> {
     return match log_level.to_lowercase().as_str() {
@@ -37,27 +41,88 @@ fn main() {
 
     log::set_max_level(string_to_level_filter(&config.log_level).unwrap_or(LevelFilter::Trace));
 
+    if init_state(&config.devices).is_err() {
+        panic!("Unable to initialize state management!");
+    }
+
+    let initial_state = get_state();
+
+    // Check if UPS was on battery prior to FSD
+    let mut on_battery = false;
+    let mut restoring = false;
+    let mut restoration_start: Option<SystemTime> = None;
+    if initial_state.ups.currently_on_battery {
+        info!("Service is restoring from a UPS outage");
+        restoring = true;
+    }
+
     let interval = Duration::from_secs(config.nut.polling_interval.into());
     let mut next_time = Instant::now() + interval;
 
     loop {
-        let ups_status = get_ups_status(
+        let _ups_status = get_ups_status(
             &config.nut.ups_name,
             &config.nut.host,
             &config.nut.username,
             &config.nut.password,
         );
 
-        if ups_status.is_err() {
-            error!(target: "UPS", "Unable to establish connection to {}@{}", config.nut.ups_name, config.nut.host)
-        } else {
-            let UPSStatus {
-                is_on_battery: ups_is_on_battery,
-                battery_percentage: ups_battery_percentage,
-                load_percentage: ups_load_percentage,
-            } = ups_status.unwrap();
-            debug!(target: "UPS", "UPS Status: {} | UPS Load: {ups_load_percentage}% | UPS Battery: {ups_battery_percentage}%", if ups_is_on_battery { "ON BAT" } else { "ONLINE" })
+        if _ups_status.is_err() {
+            error!(target: "UPS", "Unable to establish connection to {}@{}", config.nut.ups_name, config.nut.host);
+            continue;
         }
+
+        let ups_status = _ups_status.unwrap();
+
+        let UPSStatus {
+            currently_on_battery: ups_currently_on_battery,
+            battery_percentage: ups_battery_percentage,
+            load_percentage: ups_load_percentage,
+        } = ups_status;
+        let _ = update_ups_state(ups_status);
+
+        debug!(target: "UPS", "UPS Status: {} | UPS Load: {ups_load_percentage}% | UPS Battery: {ups_battery_percentage}%", if ups_currently_on_battery { "ON BAT" } else { "ONLINE" });
+
+        // Check if devices are online
+        for device in &config.devices {
+            let _ =
+                mark_device_online(device.friendly_name.clone(), is_device_online(&device.host));
+        }
+
+        if ups_currently_on_battery {
+            let _ = mark_online_devices();
+
+            if !on_battery {
+                info!(target: "UPS", "UPS switched to battery power");
+            }
+
+            on_battery = true;
+        } else if restoring || (on_battery && !ups_currently_on_battery) {
+            restoring = true;
+            on_battery = false;
+
+            if restoration_start.is_none() {
+                restoration_start = Some(SystemTime::now());
+                info!(target: "UPS", "UPS switched to AC power");
+            }
+
+            let restoration_time_elapsed = restoration_start.unwrap().elapsed().unwrap();
+
+            if restoration_time_elapsed < Duration::from_secs(config.wol.restore_delay.into()) {
+                warn!(
+                    "Waiting {} more seconds before waking devices",
+                    (config.wol.restore_delay as u64) - restoration_time_elapsed.as_secs()
+                );
+            } else if ups_battery_percentage < config.wol.min_battery_percentage {
+                warn!(
+                    "Waiting for battery to reach minimum percentage before waking devices ({}%/{}%)",
+                    ups_battery_percentage, config.wol.min_battery_percentage
+                );
+            } else {
+            }
+        }
+
+        let _ = save_state();
 
         sleep(next_time - Instant::now());
         next_time += interval;
--- src/state.rs
+++ src/state.rs
@@ -1,25 +1,89 @@
 use std::{
     fs::File,
-    io::{Result, Write},
+    io::{Error, ErrorKind, Read, Result, Write},
     path::Path,
+    sync::Mutex,
 };
 
 use bincode::{Decode, Encode, config};
 
-use crate::monitoring::UPSStatus;
+use crate::{config::DeviceConfig, monitoring::UPSStatus};
 
-#[derive(Encode, Decode, Debug)]
+#[derive(Encode, Decode, Debug, Clone)]
 pub struct NutjobState {
     pub ups: UPSStatus,
-    pub devices: DeviceStatus,
+    pub devices: Vec<DeviceState>,
 }
 
-#[derive(Encode, Decode, Debug)]
-pub struct DeviceStatus {
+#[derive(Encode, Decode, Debug, Clone)]
+pub struct DeviceState {
     pub friendly_name: String,
     pub online_before_shutdown: bool,
     pub online: bool,
     pub wol_sent: bool,
+}
+
+static STATE_PATH: &'static str = "/nutjob/state";
+
+static STATE: Mutex<NutjobState> = Mutex::new(NutjobState {
+    ups: UPSStatus {
+        currently_on_battery: false,
+        battery_percentage: 100,
+        load_percentage: 0,
+    },
+    devices: Vec::new(),
+});
+
+pub fn read_vector(path: &Path) -> Result<Vec<u8>> {
+    let mut file = File::open(path)?;
+    let mut data = vec![];
+    file.read_to_end(&mut data)?;
+    return Ok(data);
+}
+
+pub fn read_state_from_file() -> NutjobState {
+    let config = config::standard();
+
+    let vector = read_vector(Path::new(STATE_PATH)).unwrap_or_default();
+
+    let (decoded, _len): (NutjobState, usize) = bincode::decode_from_slice(&vector, config)
+        .unwrap_or((
+            NutjobState {
+                ups: UPSStatus {
+                    currently_on_battery: false,
+                    battery_percentage: 100,
+                    load_percentage: 0,
+                },
+                devices: Vec::new(),
+            },
+            0,
+        ));
+
+    return decoded;
+}
+
+pub fn init_state(device_configs: &Vec<DeviceConfig>) -> Result<()> {
+    let mut state = read_state_from_file();
+
+    state.devices = device_configs
+        .into_iter()
+        .map(|device| DeviceState {
+            friendly_name: device.friendly_name.clone(),
+            online_before_shutdown: false,
+            online: false,
+            wol_sent: false,
+        })
+        .collect();
+
+    update_state(state)?;
+
+    return save_state();
+}
+
+pub fn get_state() -> NutjobState {
+    let guard = STATE.lock().unwrap();
+
+    return guard.clone();
 }
 
 fn save_vector(path: &Path, encoded: &[u8]) -> Result<()> {
@@ -28,10 +92,85 @@ fn save_vector(path: &Path, encoded: &[u8]) -> Result<()> {
     return Ok(());
 }
 
-pub fn save_state(state: &NutjobState) -> Result<()> {
+pub fn save_state() -> Result<()> {
     let config = config::standard();
 
-    let encoded = bincode::encode_to_vec(state, config).unwrap();
+    let guard = STATE.lock().unwrap();
+    let encoded = bincode::encode_to_vec(guard.clone(), config).unwrap();
 
-    return save_vector(Path::new("/nutjob/state"), &encoded);
+    return save_vector(Path::new(STATE_PATH), &encoded);
+}
+
+pub fn update_state(new_state: NutjobState) -> Result<()> {
+    let mut guard = STATE.lock().unwrap();
+
+    *guard = new_state;
+
+    return Ok(());
+}
+
+pub fn update_device_state(new_device_state: DeviceState) -> Result<()> {
+    let state = get_state();
+
+    return update_state(NutjobState {
+        ups: state.ups,
+        devices: state
+            .devices
+            .clone()
+            .into_iter()
+            .map(|device| {
+                if device.friendly_name == new_device_state.friendly_name {
+                    new_device_state.clone()
+                } else {
+                    device
+                }
+            })
+            .collect(),
+    });
+}
+
+pub fn mark_device_online(friendly_name: String, online: bool) -> Result<()> {
+    let state = get_state();
+
+    let _device = state
+        .devices
+        .into_iter()
+        .find(|device| device.friendly_name == friendly_name);
+
+    match _device {
+        Some(mut device) => {
+            device.online = online;
+
+            return update_device_state(device);
+        }
+        None => Err(Error::new(ErrorKind::InvalidInput, "Device not found")),
+    }
+}
+
+pub fn mark_online_devices() -> Result<()> {
+    let state = get_state();
+
+    return update_state(NutjobState {
+        ups: state.ups,
+        devices: state
+            .devices
+            .clone()
+            .into_iter()
+            .map(|device| DeviceState {
+                friendly_name: device.friendly_name,
+                online_before_shutdown: device.online,
+                online: device.online,
+                wol_sent: device.wol_sent,
+            })
+            .collect(),
+    });
+}
+
+pub fn update_ups_state(new_ups_state: UPSStatus) -> Result<()> {
+    let state = get_state();
+
+    return update_state(NutjobState {
+        ups: new_ups_state.clone(),
+        devices: state.devices.clone(),
+    });
 }
