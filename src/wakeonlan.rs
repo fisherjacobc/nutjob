8b9de9a8ad4494a7bf2c938d01e22597938b30b9
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2025-07-18 22:16:09 -0400

    send back Result of wakeonlan

--- src/wakeonlan.rs
+++ src/wakeonlan.rs
@@ -1,8 +1,9 @@
 use log::{debug, error, info};
+use std::io::Result;
 use std::net::Ipv4Addr;
 use std::str::FromStr;
 
-pub fn wakeonlan(mac: &str, friendly_name: &str) {
+pub fn wakeonlan(mac: &str, friendly_name: &str) -> Result<()> {
     let mac_address = wol::MacAddr6::from_str(mac).unwrap();
 
     info!(target: "WoL", "Attempting to wake {friendly_name}");
@@ -10,7 +11,13 @@ pub fn wakeonlan(mac: &str, friendly_name: &str) {
     let wol_result = wol::send_magic_packet(mac_address, None, (Ipv4Addr::BROADCAST, 9).into());
 
     match wol_result {
-        Ok(()) => info!(target: "WoL", "Sent packet to {friendly_name} successfully"),
-        Err(e) => error!(target: "Wol", "Failed to send packet to {friendly_name}: {e}"),
+        Ok(()) => {
+            info!(target: "WoL", "Sent packet to {friendly_name} successfully");
+            return Ok(());
+        }
+        Err(e) => {
+            error!(target: "WoL", "Failed to send packet to {friendly_name}: {e}");
+            return Err(e);
+        }
     }
 }
